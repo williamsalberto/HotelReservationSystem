01b654a0a4caa5c75809673f4a5d59d6121f1e44
Alberto C. Williams A <142279168+williamsalberto@users.noreply.github.com>
2024-04-30 13:11:50 -0400

    Add files via upload

---
+++ reservador.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import QApplication
+from login import Login
+
+class Reservador():
+    def __init__(self):
+        self.app = QApplication([])
+        self.login = Login()
+        self.app.exec()
+        
