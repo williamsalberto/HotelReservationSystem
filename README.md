95d1434af3560fba56c0aa2237896f1143a197f3
Jelenio <merlinjesu26@gmail.com>
2024-07-15 22:46:46 -0400

    Eliminar todo el contenido de la rama main

--- README.md
+++
@@ -1,2 +0,0 @@
-# HotelReservationSystem
-Reservation system for a hotel as part of our community service, using Python as a development language along with its PyQt5 library and PostgreSQL as a database manager.
--- reservador.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QApplication
-from gui.login import Login
-
-class Reservador():
-    def __init__(self):
-        self.app = QApplication([])
-        self.login = Login()
-        self.app.exec()
